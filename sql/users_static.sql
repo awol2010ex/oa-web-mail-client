e05b9104013962a6108fb5eb5c5f3b3eac413cf1
awol2010ex <awol2010ex@gmail.com>
2013-03-29 09:55:56 +0800

    用户容量表同步SQL

--- sql/users_static.sql
+++ sql/users_static.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users_static ( username VARCHAR(32) NOT NULL, totalcapacity BIGINT NOT NULL, PRIMARY KEY (username) ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+delete from users_static;
 
-
-
+insert into users_static (totalcapacity ,username)
 SELECT
     SUM(OCTET_LENGTH( t.message_body)),
     t.repository_name
