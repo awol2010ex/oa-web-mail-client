c15fa9260df9a90281676d456908cb249c29e5cd
awol2010ex <awol2010ex@gmail.com>
2013-04-01 09:23:20 +0800

    账户容量表相关SQL

---
+++ sql/users_static-db2.sql
@@ -0,0 +1,11 @@
+CREATE TABLE users_static ( username VARCHAR(32) NOT NULL, totalcapacity BIGINT NOT NULL, PRIMARY KEY (username) ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+delete from users_static;
+
+insert into users_static (totalcapacity ,username)
+SELECT
+    SUM(LENGTH( t.message_body)),
+    t.repository_name
+FROM
+    inbox t
+GROUP BY
+    t.repository_name;
